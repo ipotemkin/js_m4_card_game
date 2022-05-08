59e794a658028f9d2b4bc6e68380e56cb6f42626
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2022-05-09 00:54:32 +0300

    added screens 2&3

---
+++ src/templates.js
@@ -0,0 +1,25 @@
+function cardBack() {
+  return {
+    tag: 'div',
+    cls: 'card-back',
+    content: [
+      {
+        tag: 'div',
+        cls: 'card-back__img',
+      },
+    ],
+  };
+}
+
+function cardFront(image) {
+  const url = '../src/img/' + image;
+  return {
+    tag: 'div',
+    cls: 'card-back',
+    attrs: {
+      style: `background: url('${url}') no-repeat center;`,
+    },
+  };
+}
+
+module.exports = { cardBack, cardFront };
